a672cb136407172ae1a7e703e9ea1c78461df728
Sakibur192 <sakibur521@gmail.com>
2026-01-14 21:35:13 +0600

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,41 @@
+import org.gradle.internal.impldep.org.jsoup.safety.Safelist.basic
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven(url = "https://jitpack.io")
+
+
+
+
+
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+
+        maven {
+            url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
+            // Do not change the username below. It should always be "mapbox" (not your username).
+            credentials.username = "mapbox"
+            // Use the secret token stored in gradle.properties as the password
+            credentials.password = providers.gradleProperty("MAPBOX_DOWNLOADS_TOKEN").get()
+            authentication.create<BasicAuthentication>("basic")
+        }
+
+
+
+
+
+    }
+}
+
+rootProject.name = "Bariyala"
+include(":app")
+ 
